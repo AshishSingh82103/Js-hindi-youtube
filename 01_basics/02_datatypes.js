10829f2c5865aa3ad6ab6bce93d33886521a5a50
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2023-10-09 07:32:32 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,2 +1,31 @@
 "use strict"; //treat all js code as a newer version
-alert(3 + 3) // we are usingh node js, not browser
+// alert(3 + 3) // we are using node js, not browser(alert is not defined)
+
+// console.log(3 + 
+//     3); //code readability should be high
+
+
+console.log("ashish")
+
+let name = "Ashish";
+let age = 27;
+let loggedIn = false;
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefiend => 
+//symbol => unique
+
+//Object
+
+console.log(typeof "ashish"); //string
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+
+
+

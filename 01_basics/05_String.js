547b095861a69d6de391296bcf7790e5d87ef9fc
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2023-10-09 18:20:33 +0000

    Done

---
+++ 01_basics/05_String.js
@@ -0,0 +1,7 @@
+const name = "Ashish";
+const repo = 50;
+
+// console.log(name + repo + " value"); // Ashish50 value
+
+// `` => backticks
+console.log(`Hello my name is ${name} and my repo count is ${repo}`); //Hello my name is Ashish and my repo count is 50 (this is good method)

ff6cd97ae9b9542e23632150a66e56f286ee483e
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2023-10-09 11:08:08 +0000

    comparision

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1); // true
+// console.log(2 >= 1); // true
+// console.log(2 < 1); // false
+// console.log(2 == 1); // flase
+// console.log(2 != 1); // true
+
+// console.log("2" > 1); // ture
+// console.log("02" > 1); //true
+
+// console.log(null > 0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true
+
+// console.log(undefined == 0); // false
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
+
+//=======>  === is not check only value it,s also check the data types
+console.log("2" === 2); // flase
+

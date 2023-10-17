bb73a90602a2fe97cc1b86e07b16fc94b0507eee
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2023-10-17 10:18:31 +0000

    done

--- 01_basics/05_String.js
+++ 01_basics/05_String.js
@@ -5,3 +5,13 @@ const repo = 50;
 
 // `` => backticks
 console.log(`Hello my name is ${name} and my repo count is ${repo}`); //Hello my name is Ashish and my repo count is 50 (this is good method)
+
+const gameName = new String("hitesh-hc");
+
+// console.log(gameName[0]); // h
+// console.log(gameName.__proto__); // {}
+
+// console.log(gameName.length); // 8
+// console.log(gameName.toUpperCase()); // HITESHHC
+// console.log(gameName.charAt(1)); // i
+console.log(gameName.indexOf('t')); // 2

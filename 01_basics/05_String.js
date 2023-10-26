b21714430ce5aeb3a841b58863f46f50efffc44a
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2023-10-26 07:53:49 +0000

    Date and Time

--- 01_basics/05_String.js
+++ 01_basics/05_String.js
@@ -1,34 +1,45 @@
-const name = "Ashish";
-const repo = 50;
+// Dates
 
-// console.log(name + repo + " value"); // Ashish50 value
 
-// `` => backticks
-// console.log(`Hello my name is ${name.toUpperCase()} and my repo count is ${repo}`); //Hello my name is Ashish and my repo count is 50 (this is good method)
-const gameName = new String("hitesh-hc");
+let myDates = new Date();
+// console.log(myDates); // 2023-10-25T08:06:36.114Z
+// console.log(myDates.toString()); // Wed Oct 25 2023 08:08:05 GMT+0000 (Coordinated Universal Time)
+// console.log(myDates.toISOString()); // 2023-10-25T08:08:59.039Z
+// console.log(myDates.toJSON()); // 2023-10-25T08:08:59.039Z
+// console.log(myDates.toLocaleDateString()); // 10/25/2023
+// console.log(myDates.toLocaleTimeString()); // 8:08:59 AM
+// console.log(myDates.toDateString()); // Wed Oct 25 2023
+// console.log(myDates.toLocaleString()); // 10/25/2023, 8:11:13 AM
+// console.log(typeof myDates); // Object
 
-// console.log(gameName[0]); // h
-// console.log(gameName.__proto__); // {}
+let myCreateDate = new Date(2023, 0, 23);
+// console.log(myCreateDate.toDateString()); // Mon Jan 23 2023(month satrt on 0 in java scripts)
 
-// console.log(gameName.length); // 9
-// console.log(gameName.toUpperCase()); // HITESH-HC
-// console.log(gameName.charAt(1)); // i
-// console.log(gameName.indexOf('t')); // 2
-// console.log(gameName.indexOf('i')); // 1
+let myCreateDate2 = new Date(2023, 0, 23, 5, 3);
+// console.log(myCreateDate2.toLocaleString()); // 1/23/2023, 5:03:00 AM
 
-const newString = gameName.substring(0, 4); // hite
-// console.log(newString); // hite
-const anotherString = gameName.slice(-8, 4);
-// console.log(anotherString); // ite
+let myCreateDate3 = new Date("2023-01-14");
+// console.log(myCreateDate3.toLocaleString()); // 14/1/2023, 5:30:00 am
 
-const newStringOne = "  hitesh  ";
-console.log(newStringOne); //  hitesh  //
-console.log(newStringOne.trim()); // hitesh
+let myCreateDate4 = new Date("01-14-2023");
+// console.log(myCreateDate4.toLocaleString()); // 14/1/2023, 12:00:00 am
 
-const url = "https://hitesh.com/hitesh%20choudhary";
-console.log(url.replace('%20', '-')); // https://hitesh.com/hitesh-choudhary
-console.log(url.includes('sundar')); // false
+let myTimeStamp = Date.now();
+console.log(myTimeStamp); // 1698263939723
+// console.log(myCreateDate4.getTime()); // 1673634600000
 
-const gameName1 = new String('hitesh-hc-com');
-console.log(gameName1.split('-')); // [ 'hitesh', 'hc', 'com' ]
+// console.log(Math.floor(Date.now()/1000)); // 1698263939 
+
+let newDate = new Date();
+console.log(newDate); // 2023-10-26T07:35:52.567Z
+console.log(newDate.getMonth()); // 9
+console.log(newDate.getDay()); // 4
+
+// `${newDate.getDay()} and the time ${newDate.getMonth()}`
+
+// in vs  get the property by use ctrl+space
+
+newDate.toLocaleString('default', {
+    weekday: "long"
+})
 

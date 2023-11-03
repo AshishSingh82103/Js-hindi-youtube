3614bcc7510b551e4cf6c9301b4f80c74f1a0625
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2023-11-03 06:59:23 +0000

    Arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -11,3 +11,42 @@ const user = {
 // user.userName = "sam";
 // user.welcomeMessage(); // sam, welcome to the website
 // console.log(this); // {} in  node plate-form but other browser showing results
+
+
+// function chai() {
+//    // console.log(this);
+//    let userName = "hitesh";
+//    console.log(this.userName); // udefined
+// }
+// chai();
+
+// const chai = function() {
+//    let userName = "hitesh";
+//    console.log(this.userName); // undefined
+
+// }
+
+// chai();
+
+// ----------------------Arrow function-----------------
+const chai = () => {
+   let username = "hitesh";
+   // console.log(this.username); // undefined
+   console.log(this);
+}
+chai();
+
+// const addTwo = (num1, num2) => {
+//    return num1 + num2;
+
+// }
+
+// const addTwo = (num1, num2) => num1 + num2;
+
+// or------- using this method in react
+const addTwo = (num1, num2) => (num1 + num2);
+
+console.log(addTwo(3, 4)); // 7
+
+const objectReturn = (num1, num2) => ({username:"hitesh"});
+console.log(objectReturn(3, 4)); // { username: 'hitesh' }

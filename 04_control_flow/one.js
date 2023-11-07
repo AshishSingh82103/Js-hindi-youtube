7b12a78d53c2160081ae8511b2070b9f252ab744
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2023-11-07 07:04:03 +0000

    One

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,1 +1,45 @@
-console.log("Ashish");
+// if
+
+// if (ture) {
+//     // code execute n this scope
+// }
+
+// if (false) {
+//     // code will not execute in this scope
+// }
+
+// const isUserLoggedIn =  true;
+
+// if(isUserLoggedIn) {
+//     console.log("run"); // run
+// }
+
+// <,  >, <=, >=, ==, !=, ===, !==
+
+// if (2 == "2") {
+//     console.log("execured");  // executed
+// }
+
+// if (2 === "2") {
+//     console.log("executed"); // not executed
+// }
+
+const isUserLoggedIn = true;
+const temperature = 40;
+
+// if (temperature < 50) {
+//     console.log("less than 50"); // less than 50
+// }
+// console.log("temprature is greater than 50"); // temprature is greater than 50
+
+// if (temperature === 40) {
+//     console.log("less than 50") // less than 50
+// } else {
+//     console.log("temperature is greater than 50")
+// }
+
+if (temperature === 41) {
+    console.log("less than 50") 
+} else {
+    console.log("temperature is greater than 50") // temperature is greater than 50
+}

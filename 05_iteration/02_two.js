271a4e18bcdf18003b969d7a32325f745dafe675
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2023-11-08 09:59:43 +0000

    loop

---
+++ 05_iteration/02_two.js
@@ -0,0 +1,33 @@
+// while lopp
+
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+
+// index++;
+// }
+
+let index = 0;
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+
+index = index + 2;
+
+}
+
+let myArr = ["batman", "flash", "superman"];
+
+let arr = 0;
+while (arr < myArr.length) {
+    console.log(`value is ${myArr[arr]}`);
+    arr++;
+}
+
+
+// do while loop at leat give one value
+
+let score = 1;
+do {
+   console.log(`score is ${score}`) 
+   score++;
+} while (score <= 10);

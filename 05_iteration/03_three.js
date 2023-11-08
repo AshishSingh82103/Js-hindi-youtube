84fc9552563ab32e1341ccd2763071fa389aaccc
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2023-11-08 11:27:46 +0000

    High-order array loops

---
+++ 05_iteration/03_three.js
@@ -0,0 +1,62 @@
+// for of loop
+
+// ["", "", "", ""]
+
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5];
+for (const iterator of arr) {
+    // console.log(iterator);
+    
+}
+
+const greeting = "Hello world!";
+for (const greet of greeting) {
+    // console.log(`Each char is ${greet}`)
+    
+}
+
+// maps
+const map1 = new Map();
+
+map1.set('a', 1);
+map1.set('b', 2);
+map1.set('c', 3);
+
+// console.log(map1); // Map(3) { 'a' => 1, 'b' => 2, 'c' => 3 }
+
+// console.log(map1.get('a'));
+// Expected output: 1
+
+map1.set('a', 97);
+
+// console.log(map1.get('a'));
+// Expected output: 97
+
+// console.log(map1.size);
+// Expected output: 3
+
+map1.delete('b');
+
+// console.log(map1.size);
+// Expected output: 2
+
+const map = new Map();
+
+map.set('In', "India");
+map.set('USA', "United Sate america");
+map.set('Fr', "france");
+// map.set('In', "India");
+
+// console.log(map); // Map(3) 'In' => 'India','USA' => 'United Sate america','Fr' => 'france' }
+
+// for (const key of map) {
+//     console.log(key); // [ 'In', 'India' ] [ 'USA', 'United Sate america' ] [ 'USA', 'United Sate america' ]
+// }
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);      // In :- India  
+    //                                 //  USA :- United Sate america
+    //                                  // Fr :- france
+    
+}

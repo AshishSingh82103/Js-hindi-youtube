bf86d5896864625e9a4a5d94ee386198dc5a9b72
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2023-11-09 11:26:19 +0000

    For each

---
+++ 05_iteration/05_five.js
@@ -0,0 +1,45 @@
+const coding = ["js", "ruby", "java", "python", "cpp"];
+
+// call back function => function not name
+// coding.forEach( function (item){
+//     console.log(item);
+
+// });
+
+// represent as arrow function
+// coding.forEach( (item) => {
+//     console.log(item);
+// })
+
+
+// function printMe(item) {
+//     console.log(item)
+// }
+
+// coding.forEach(printMe);
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr)
+
+// })
+
+const myCoding = [
+    {
+        languageName : "javascript",
+        languageFileName : "js",
+    },
+
+    {
+        languageName : "java",
+        languageFileName : "java",
+    },
+
+    {
+        languageName : "python",
+        languageFileName : "py", 
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+})

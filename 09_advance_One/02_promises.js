f91da638eda8cbe829ba3cd56807e0d6fcdb6ad4
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2023-12-15 09:51:18 +0000

    Promise

--- 09_advance_One/02_promises.js
+++ 09_advance_One/02_promises.js
@@ -5,4 +5,28 @@ fetch('https://something.com').then().catch().finally()
 // how to make promise is a object
 // promise no. 1
 
-const promiseOne = new promise()
+const promiseOne = new Promise(function(resolve, reject){
+    // Do an Async task
+    //Data base calls, cryptography, network
+    setTimeout(function() {
+        console.log('Async task is compelete'); // Async task is compelete
+        // resolve connect with then after then will execute
+        resolve();
+    }, 1000);
+    });
+    
+    promiseOne.then(function() {
+        console.log("Promised consumed"); // Promised consumed
+    })
+    
+    // other way to write the promise withou variable
+    new Promise(function(resolve, reject) {
+        setTimeout(function(){
+            console.log("Async Task 2");
+            resolve();
+        }, 1000)
+    }).then(function() {
+        console.log("Async Task 2 is resolve")
+    })
+    
+

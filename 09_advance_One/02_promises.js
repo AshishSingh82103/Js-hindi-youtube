4edaa1b5a717ce7ae05a7b91a7606838b509eec8
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2023-12-15 08:21:15 +0000

    Promise

---
+++ 09_advance_One/02_promises.js
@@ -0,0 +1,8 @@
+// console.log('ashish');
+// response is in then() and error is the catch() and is better tutorial is in finally()
+fetch('https://something.com').then().catch().finally()
+
+// how to make promise is a object
+// promise no. 1
+
+const promiseOne = new promise()

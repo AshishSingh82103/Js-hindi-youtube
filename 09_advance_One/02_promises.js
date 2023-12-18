73663c2172147282627a676ab0b66a71594fd145
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2023-12-18 09:17:13 +0000

    promise

--- 09_advance_One/02_promises.js
+++ 09_advance_One/02_promises.js
@@ -1,4 +1,4 @@
-// console.log('ashish');
+// // console.log('ashish');
 // response is in then() and error is the catch() and is better tutorial is in finally()
 fetch('https://something.com').then().catch().finally()
 
@@ -9,24 +9,62 @@ const promiseOne = new Promise(function(resolve, reject){
     // Do an Async task
     //Data base calls, cryptography, network
     setTimeout(function() {
-        console.log('Async task is compelete'); // Async task is compelete
+        // console.log('Async task is compelete'); // Async task is compelete
         // resolve connect with then after then will execute
         resolve();
     }, 1000);
     });
     
     promiseOne.then(function() {
-        console.log("Promised consumed"); // Promised consumed
+        // console.log("Promised consumed"); // Promised consumed
     })
     
-    // other way to write the promise withou variable
+    // other way to write the promise withou variable => second promise
     new Promise(function(resolve, reject) {
         setTimeout(function(){
-            console.log("Async Task 2");
+            // console.log("Async Task 2");
             resolve();
         }, 1000)
     }).then(function() {
-        console.log("Async Task 2 is resolve")
+        // console.log("Async Task 2 is resolve")
     })
+
+
+// third promise = >  how to get the value
+
+const promiseThree = new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        resolve({userName:"chai", email: "chai@example.com"})
+    }, 1000)
+})
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+// fourth promise
+const promiseFour = new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        let error = false;
+        if(!error) {
+            resolve({username:"Ashish", password: "123"})
+        } else {
+            reject('error something went wrong');
+        }
+
+    }, 1000)
+})
+
+promiseFour
+.then((user) => {
+    console.log(user);
+    return user.username
+})
+.then((username) =>{
+    console.log(username);
+
+})
+.catch(function(error){
+    console.log(error);
+})
     
 

3e52f30da68d5debee90cea0e4cc1c6f21974e7a
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2024-02-13 13:34:11 +0000

    Complete

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -18,3 +18,46 @@
 //=======>  === is not check only value it,s also check the data types
 console.log("2" === 2); // flase
 
+// =====================================================
+// primitve(Stack), Heap(Non-primitve)
+// primitve => String, number, null, undefiend, BigInt, symbol, boolean.
+// Non-primitve(reference) => Array, function, object
+
+const str = "Ashish";
+const num = 123;
+const outSideTemp = null;
+const playGame = undefined;
+const bigInt = 49253255325n;
+const indication = Symbol('123');
+const indication1 = Symbol('123');
+const score1 = true;
+console.log(indication == indication1); // false
+
+// non-primitive(Refernce)
+// -----------Array-------
+ const arr = ["capitan", "number-9", "number-8"];
+
+
+// ------------Function-----------------
+const value1 = {
+    name: "Ashish",
+    id: "123",
+    password: "ashish@12345"
+}
+
+// -----------Object------------
+const myFunction = function () {
+    console.log("Hello Number");
+}
+
+console.log(typeof str); // string
+console.log(typeof num); // number
+console.log(typeof outSideTemp); // object
+console.log(typeof playGame); // undefined
+console.log(typeof bigInt); // bigint
+console.log(typeof indication); // symbol
+console.log(typeof score1); // boolean
+
+
+
+

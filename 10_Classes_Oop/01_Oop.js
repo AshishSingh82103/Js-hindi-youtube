a6cbe977e5e9f7c21c8bba0516bc388791057a8e
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2024-03-07 07:42:52 +0000

    Opps

--- 10_Classes_Oop/01_Oop.js
+++ 10_Classes_Oop/01_Oop.js
@@ -1,12 +1,51 @@
-// ------Object-literal
+// -------------Object-literal--------------------
 const user = {
     username: "Ashish",
     loginCount: 8,
     signedIn: true,
 
     getUserDetails: function(){
-        console.log("Got user details from database");
+        // console.log("Got user details from database");
+        // console.log(`username: ${this.username}`);
+        console.log(this);
     }
 }
-console.log(user.loginCount);
-console.log(user.getUserDetails());
+// console.log(user.loginCount);
+// console.log(user.getUserDetails());
+// console.log(this); //{}
+
+// ------------------Consturctor function--------------
+// const promiseOne = new Promise();
+// const date = new Date();
+
+function User(username, logincount, isLoggedIn) {
+    this.username = username;
+    this.loginCount = logincount;
+    this.isLoggedIn = isLoggedIn;
+
+    this.greeting = function() {
+        console.log(`Welcome ${this.username}`)
+    }
+
+    return this;
+}
+const userOne =  new User("ashish", 12, true);
+const userTwo =  new User("javascript", 11, false);
+// console.log(userOne);
+// console.log(userTwo);
+console.log(userOne.constructor); // [Function: User]
+
+//------------- JavaScript Demo: Expressions - instanceof------------------
+
+function Car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  const auto = new Car('Honda', 'Accord', 1998);
+  
+  console.log(auto instanceof Car);
+  // Expected output: true
+  
+  console.log(auto instanceof Object);
+  // Expected output: true
